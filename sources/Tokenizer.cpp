27f5455f4ea98a4ee1ab1650a2b7934efc523d4e
Ellana42 <mtmrkaploun@gmail.com>
2023-03-10 16:51:56 +0100

    Tokenizer finished but untested

---
+++ sources/Tokenizer.cpp
@@ -0,0 +1,109 @@
+#include "../includes/Tokenizer.hpp"
+
+Tokenizer::Tokenizer( std::string raw_content ) : content( raw_content ),
+	state( Param ), current_position( 0 ) {}
+
+void Tokenizer::tokenize( void )
+{
+	while ( current_char() != '\0' )
+	{
+		if ( state == Param )
+		{
+			tokenize_param();
+		}
+		else if ( state == Space )
+		{
+			tokenize_space();
+		}
+		else if ( state == LongParam )
+		{
+			tokenize_long_param();
+		}
+	}
+}
+
+void Tokenizer::tokenize_param( void )
+{
+	switch ( current_char() )
+	{
+		case ' ':
+			change_state( Space );
+			if ( !buffer.empty() )
+			{
+				push_token();
+			}
+			move();
+			break;
+		case '\0':
+			if ( !buffer.empty() )
+			{
+				push_token();
+			}
+			break;
+		default:
+			accumulate();
+	}
+}
+
+void Tokenizer::tokenize_space( void )
+{
+	switch ( current_char() )
+	{
+		case ' ':
+			move();
+			break;
+		case '\0':
+			break;
+		case ':':
+			change_state( LongParam );
+			move();
+			break;
+		default:
+			change_state( Param );
+	}
+}
+
+void Tokenizer::tokenize_long_param( void )
+{
+	switch ( current_char() )
+	{
+		case '\0':
+			push_token();
+			break;
+		default:
+			accumulate();
+	}
+}
+
+char Tokenizer::current_char( void ) const
+{
+	if ( current_position < content.size() )
+	{
+		return ( content[current_position] );
+	}
+	return ( '\0' );
+}
+
+void Tokenizer::accumulate( void )
+{
+	content.push_back( current_char() );
+	move();
+}
+
+void Tokenizer::move( void )
+{
+	current_position++;
+}
+
+void Tokenizer::change_state( State new_state )
+{
+	state = new_state;
+}
+
+void Tokenizer::push_token( void )
+{
+	tokens.push_back( buffer );
+	buffer.clear();
+}
+
+Tokenizer::~Tokenizer() {}
