743b810fd51491e95f5d850c6bd475eaa155ca58
Ellana42 <mtmrkaploun@gmail.com>
2023-03-14 11:30:58 +0100

    Preliminary verificaitons on messages

--- includes/Message.hpp
+++ includes/Message.hpp
@@ -3,57 +3,15 @@
 
 #include "ft_irc.hpp"
 
-class Message;
-
-typedef void ( Message::*parse_method )( char * );
+#define MAX_SIZE 512
+#define MIN_SIZE 2
 
 class Message
 {
 	private:
 
-		std::string command;
-
-		std::string content;
-
-		std::string nickname;
-		std::string message;
-		std::string msgtarget;
-
-		std::string user;
-		std::string mode;
-		std::string unused;
-		std::string realname;
-
-		std::list<std::string> channels;
-		std::list<std::string> clients;
-		std::list<std::string> names;
-
-		std::map<std::string, parse_method> accepted_commands;
-
 	public:
 		Message( std::string content );
 		virtual ~Message();
-
-		void parse( void );
-		void set_command( std::string command );
-		void set_nickname( std::string value );
-		void parse_nick( char * str );
-		void parse_user( char * str );
-		void set_user( std::string value );
-		void set_mode( std::string value );
-		void set_unused( std::string value );
-		void set_realname( std::string value );
-		void parse_privmsg( char * str );
-		void set_message( std::string value );
-		void set_msgtarget( std::string value );
-		std::string get_nickname( void );
-		std::string get_message( void );
-		std::string get_msgtarget( void );
-		std::string get_realname( void );
-		std::string get_mode( void );
-		std::string get_unused( void );
-		std::string get_content( void );
-		std::string get_user( void );
-		std::string get_command( void );
 };
 #endif /* MESSAGE_H */
--- sources/Message.cpp
+++ sources/Message.cpp
@@ -1,140 +1,22 @@
 #include "../includes/Message.hpp"
+#include <stdexcept>
 
 Message::Message( std::string raw_message )
 {
-	content = raw_message;
-	/* accepted_commands["NICK"] = &Message::parse_nick; */
-	/* accepted_commands["USER"] = &Message::parse_user; */
-	/* accepted_commands["PRIVMSG"] = &Message::parse_privmsg; */
-	parse();
+	if ( raw_message.size() > MAX_SIZE )
+	{
+		throw std::invalid_argument( "Message too long" );
+	}
+	if ( raw_message.size() < MIN_SIZE )
+	{
+		throw std::invalid_argument( "Message too short" );
+	}
+	if ( !( raw_message[raw_message.size() - 1] == '\r'
+	        && raw_message[raw_message.size()] == '\n' ) )
+	{
+		throw std::invalid_argument( "Invalid message end" );
+	}
+	raw_message.resize( raw_message.size() - 2 );
 }
 
 Message::~Message( void ) {};
-
-void Message::parse( void )
-{
-	char *parsing = ( char * )content.c_str();
-
-	set_command( std::strtok( parsing, " " ) );
-	if ( command == "NICK" )
-	{
-		parse_nick( parsing );
-	}
-	else if ( command == "USER" )
-	{
-		parse_user( parsing );
-	}
-	else if ( command == "PRIVMSG" )
-	{
-		parse_privmsg( parsing );
-	}
-}
-
-void Message::set_command( std::string value )
-{
-	command = value;
-}
-
-void Message::set_nickname( std::string value )
-{
-	nickname = value;
-}
-
-void Message::parse_nick( char *parsing )
-{
-	( void ) parsing;
-	set_nickname( std::strtok( NULL, "\r" ) );
-}
-
-void Message::parse_user( char *parsing )
-{
-	( void ) parsing;
-	set_user( std::strtok( NULL, " " ) );
-	set_mode( std::strtok( NULL, " " ) );
-	set_unused( std::strtok( NULL, " " ) );
-	set_realname( std::strtok( NULL, "\r" ) );
-	realname.erase( 0, 1 );
-}
-
-void Message::set_user( std::string value )
-{
-	user = value;
-}
-
-void Message::set_mode( std::string value )
-{
-	mode = value;
-}
-
-void Message::set_unused( std::string value )
-{
-	unused = value;
-}
-
-void Message::set_realname( std::string value )
-{
-	realname = value;
-}
-
-void Message::parse_privmsg( char *parsing )
-{
-	( void ) parsing;
-	set_msgtarget( std::strtok( NULL, " " ) );
-	set_message( std::strtok( NULL, "\r" ) );
-	message.erase( 0, 1 );
-}
-
-void Message::set_message( std::string value )
-{
-	message = value;
-}
-
-void Message::set_msgtarget( std::string value )
-{
-	msgtarget = value;
-}
-
-std::string Message::get_nickname( void )
-{
-	return ( nickname );
-}
-
-std::string Message::get_message( void )
-{
-	return ( message );
-}
-
-std::string Message::get_msgtarget( void )
-{
-	return ( msgtarget );
-}
-
-std::string Message::get_realname( void )
-{
-	return ( realname );
-}
-
-std::string Message::get_mode( void )
-{
-	return ( mode );
-}
-
-std::string Message::get_unused( void )
-{
-	return ( unused );
-}
-
-std::string Message::get_content( void )
-{
-	return ( content );
-}
-
-std::string Message::get_user( void )
-{
-	return ( user );
-}
-
-std::string Message::get_command( void )
-{
-	return ( command );
-}
