a7445abf489c080a7ceba89a794f5afbcecb23d6
Ellana42 <mtmrkaploun@gmail.com>
2023-03-29 13:51:10 +0200

    Parsing modes

---
+++ sources/Mode_Parsing.cpp
@@ -0,0 +1,98 @@
+#include "Mode_Parsing.hpp"
+#include <cctype>
+#include <exception>
+
+ModeParsing::ModeParsing( std::string modestring ) : modestring( modestring ),
+	current_state( PlusMinus ), position( 0 ) {}
+
+ModeParsing::~ModeParsing() {}
+
+void ModeParsing::parse( void )
+{
+	while ( position < modestring.size() )
+	{
+		switch ( current_state )
+		{
+			case PlusMinus:
+				parse_plus_minus();
+				break;
+			case ModeChar:
+				parse_mode_char();
+				break;
+		}
+	}
+}
+
+void ModeParsing::parse_plus_minus( void )
+{
+	if ( get_char() == '+' )
+	{
+		current_mode = Plus;
+		current_state = ModeChar;
+		move();
+	}
+	else if ( get_char() == '-' )
+	{
+		current_mode = Minus;
+		current_state = ModeChar;
+		move();
+	}
+	else
+	{
+		throw InvalidModestringException();
+	}
+
+}
+
+void ModeParsing::parse_mode_char( void )
+{
+	if ( std::isalpha( get_char() ) )
+	{
+		if ( current_mode == Plus )
+		{
+			added_modes.push_back( get_char() );
+			move();
+		}
+		else if ( current_mode == Minus )
+		{
+			removed_modes.push_back( get_char() );
+			move();
+		}
+	}
+	else if ( get_char() == '-' || get_char() == '+' )
+	{
+		current_state = PlusMinus;
+	}
+	else
+	{
+		throw InvalidModestringException();
+	}
+}
+
+void ModeParsing::move( void )
+{
+	position++;
+}
+
+char ModeParsing::get_char( void )
+{
+	if ( position < modestring.size() )
+	{
+		return ( modestring[position] );
+	}
+	else
+	{
+		throw std::out_of_range( "Requested position is beyond modestring" );
+	}
+}
+
+
+std::string ModeParsing::get_added_modes( void )
+{
+	return ( added_modes );
+}
+
+std::string ModeParsing::get_removed_modes( void )
+{
+	return ( removed_modes );
+}
