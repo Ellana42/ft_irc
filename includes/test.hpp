f468e7f1143ff1cd299e67f28497c6c472af3c23
mcombeau <mcombeau@student.42.fr>
2023-03-30 15:27:02 +0200

    add and rework tests

--- includes/test.hpp
+++ includes/test.hpp
@@ -7,10 +7,15 @@
 #include "Colors.h"
 
 void run_test_suite( void );
-void test_user_registration( Context & context );
+void test_preregistration_security( Context & context );
+void test_register_users( Context & context );
+void test_user_before_registration( Context & context );
+void test_user_after_registration( Context & context );
+void test_nick( Context & context );
+void test_user( Context & context );
 void test_admin_info( Context & context );
 void test_summon_users( Context & context );
-void test_simple_privmsg( Context & context );
+void test_user_privmsg( Context & context );
 void test_names( Context & context );
 void test_join( Context & context );
 void test_part( Context & context );
