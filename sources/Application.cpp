54e397da3b0d245ef999c2f3c4926485af252ed7
mcombeau <mcombeau@student.42.fr>
2023-06-05 16:09:45 +0200

    debunnging

--- sources/Application.cpp
+++ sources/Application.cpp
@@ -108,6 +108,38 @@ void Application::launch_server()
 	close( server.fd );
 }
 
+void examineChar(char c){
+    if(c=='\n')
+        std::cout<<"\\n";
+    else if(c=='\r')
+        std::cout<<"\\r";
+    else if(c=='\b')
+        std::cout<<"\\b";
+    else if(c=='\t')
+        std::cout<<"\\t";
+    else if(c=='\a')
+        std::cout<<"\\a";
+    else if(c=='\v')
+        std::cout<<"\\v";
+    else if(c=='\0')
+        std::cout<<"\\0";
+    else
+        std::cout<<c;
+}
+
+void examineCharStar(char *cs){
+    size_t i;
+    for(i=0;i < std::strlen(cs);i++)
+        examineChar(cs[i]);
+}
+
+void examineString(std::string s){
+    size_t i;
+    if(s!="")
+        for(i=0;i<s.length();i++)
+            examineChar(s[i]);
+}
+
 void Application::read_message(int fd, int *num_clients)
 {
 	char buf[4096];
@@ -134,7 +166,9 @@ void Application::read_message(int fd, int *num_clients)
 			break;
 		}
 		message_buffer += std::string(buf);
-		std::cerr << "- STRING BUFFER contains: [" << message_buffer << "]" << std::endl;
+		std::cerr << "- STRING BUFFER contains: [";
+			examineString(message_buffer);
+			std::cerr << "]" << std::endl;
 		terminator = message_buffer.find("\r\n", 0);
 		std::cerr << "- TERMINATOR is pos :" << terminator << std::endl;
 	}
