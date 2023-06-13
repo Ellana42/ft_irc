6e05c92b1000cb1707cb85721838caed34e552db
mcombeau <mcombeau@student.42.fr>
2023-06-13 16:31:20 +0200

    add multiple simultaneous connections test script

---
+++ test/flood-script-multiple-clients.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo "Please specify a port between 6660 and 7000"
+	echo "Usage: ./test-flood-script.sh <port> <number_of_clients>"
+	exit 1
+fi
+
+PORT=$1
+PORT_STATUS=$(nc -w5 -z -v localhost $PORT 2>&1)
+
+if [[ ${PORT} -lt 6000 ]] || [[ ${PORT} -gt 7000 ]]
+then
+	echo "Invalid port: port must be between 6660 and 7000"
+	echo "Usage: ./test-flood-script.sh <port> <number_of_clients>"
+	exit 1
+elif [[ ${PORT_STATUS} != *"open"* ]]
+then
+	echo "Port status: ${PORT_STATUS}"
+	exit 1
+fi
+
+NB_CLIENTS=$(($2))
+if [ -z $2 ] 
+then
+	echo "Please specify the number of clients to simultaneously connect to the port"
+	echo "Usage: ./test-flood-script.sh <port> <number_of_clients>"
+	exit 1
+elif [[ ${NB_CLIENTS} -lt 1 ]] || [[ ${NB_CLIENTS} -gt 1000 ]]
+then
+	echo "Invalid number of clients: min 1, max 1000."
+	echo "Usage: ./test-flood-script.sh <port> <number_of_clients>"
+	exit 1
+fi
+
+while [[ ${NB_CLIENTS} -ne 0 ]]
+do
+	echo
+	echo "-- Connecting a new client #${NB_CLIENTS}"
+	echo "-- Registering IRC client with nickname: nick_${NB_CLIENTS}"
+	SEND_IRC_COMMANDS="print \"USER a a a a\r\n\" . \"NICK nick_${NB_CLIENTS}\r\n\""
+	perl -e "$SEND_IRC_COMMANDS" | timeout -k 1 10s nc localhost $PORT &>/dev/null &
+	NB_CLIENTS=$((${NB_CLIENTS} - 1))
+done
+
+wait
+echo
+echo "-- Closed all client connections"
