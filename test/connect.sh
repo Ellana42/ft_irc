9fa096ca4dbed10ed34b2210758f31a343f34a07
mcombeau <mcombeau@student.42.fr>
2023-06-21 17:39:23 +0200

    add autoconnect with password

--- test/connect.sh
+++ test/connect.sh
@@ -11,18 +11,25 @@ CYAN='\033[00;36m'
 if [ -z $1 ]
 then
 	echo "Please specify a port between 6660 and 7000"
-	echo "Usage: ./connect.sh <port> <nickname>"
+	echo "Usage: ./connect.sh <port> <password> <nickname>"
 	exit 1
 fi
 if [ -z $2 ]
 then
+	echo "Please specify the server password"
+	echo "Usage: ./connect.sh <port> <password> <nickname>"
+	exit 1
+fi
+if [ -z $3 ]
+then
 	echo "Please specify a nickname"
-	echo "Usage: ./connect.sh <port> <nickname>"
+	echo "Usage: ./connect.sh <port> <password> <nickname>"
 	exit 1
 fi
 
 PORT=$1
-NICK=$2
+PASS=$2
+NICK=$3
 PORT_STATUS=$(nc -w5 -z -v localhost $PORT 2>&1)
 
 if [[ ${PORT} -lt 6660 ]] || [[ ${PORT} -gt 7000 ]]
@@ -36,7 +43,8 @@ then
 	exit 1
 fi
 
-echo "USER a a a a" > /tmp/irc_commands.tmp
+echo "PASS ${PASS}" > /tmp/irc_commands.tmp
+echo "USER a a a a" >> /tmp/irc_commands.tmp
 echo "NICK ${NICK}" >> /tmp/irc_commands.tmp
 cat /tmp/irc_commands.tmp
 cat /tmp/irc_commands.tmp - | ncat -C localhost $PORT
