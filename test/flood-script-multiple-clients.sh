646bfccc91ff4415f0eaeac9e3f997c2efcd7339
mcombeau <mcombeau@student.42.fr>
2023-06-22 14:58:09 +0200

    update flood with multiple clients script

--- test/flood-script-multiple-clients.sh
+++ test/flood-script-multiple-clients.sh
@@ -3,11 +3,18 @@
 if [ -z $1 ]
 then
 	echo "Please specify a port between 6660 and 7000"
-	echo "Usage: ./test-flood-script.sh <port> <number_of_clients>"
+	echo "Usage: ./test-flood-script.sh <port> <password> <number_of_clients>"
+	exit 1
+fi
+if [ -z $2 ]
+then
+	echo "Please specify the server connection password"
+	echo "Usage: ./test-flood-script.sh <port> <password> <number_of_clients>"
 	exit 1
 fi
 
 PORT=$1
+PASS=$2
 PORT_STATUS=$(nc -w5 -z -v localhost $PORT 2>&1)
 
 if [[ ${PORT} -lt 6000 ]] || [[ ${PORT} -gt 7000 ]]
@@ -21,16 +28,16 @@ then
 	exit 1
 fi
 
-NB_CLIENTS=$(($2))
-if [ -z $2 ] 
+NB_CLIENTS=$(($3))
+if [ -z $3 ] 
 then
 	echo "Please specify the number of clients to simultaneously connect to the port"
-	echo "Usage: ./test-flood-script.sh <port> <number_of_clients>"
+	echo "Usage: ./test-flood-script.sh <port> <password> <number_of_clients>"
 	exit 1
 elif [[ ${NB_CLIENTS} -lt 1 ]] || [[ ${NB_CLIENTS} -gt 1000 ]]
 then
 	echo "Invalid number of clients: min 1, max 1000."
-	echo "Usage: ./test-flood-script.sh <port> <number_of_clients>"
+	echo "Usage: ./test-flood-script.sh <port> <password> <number_of_clients>"
 	exit 1
 fi
 
@@ -39,7 +46,7 @@ do
 	echo
 	echo "-- Connecting a new client #${NB_CLIENTS}"
 	echo "-- Registering IRC client with nickname: nick_${NB_CLIENTS}"
-	SEND_IRC_COMMANDS="print \"USER a a a a\r\n\" . \"NICK nick_${NB_CLIENTS}\r\n\""
+	SEND_IRC_COMMANDS="print \"PASS ${PASS}\r\n\" . \"USER a a a a\r\n\" . \"NICK nick_${NB_CLIENTS}\r\n\""
 	perl -e "$SEND_IRC_COMMANDS" | timeout -k 1 10s nc localhost $PORT &>/dev/null &
 	NB_CLIENTS=$((${NB_CLIENTS} - 1))
 done
